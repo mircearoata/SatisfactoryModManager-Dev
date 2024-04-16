2f6bc57d4753db963584c8ac148925949e8ff507
mircearoata <mircearoatapalade@gmail.com>
2024-04-16 16:28:25 +0200

    Add darwin autoupdate

---
+++ backend/autoupdate/update_mode_darwin.go
@@ -0,0 +1,14 @@
+package autoupdate
+
+import (
+	"github.com/satisfactorymodding/SatisfactoryModManager/backend/autoupdate/apply"
+)
+
+func init() {
+	registerUpdateType("standalone", func() UpdateType {
+		return UpdateType{
+			ArtifactName: "SatisfactoryModManager_darwin_universal.zip",
+			Apply:        apply.MakeDarwinAppApply(),
+		}
+	})
+}
