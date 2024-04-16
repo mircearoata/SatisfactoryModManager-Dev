919bba476a2927b8a65e93a9a1cab5f1ae06a308
mircearoata <mircearoatapalade@gmail.com>
2024-04-16 16:35:10 +0200

    Fix darwin update

--- backend/autoupdate/update_mode_darwin.go
+++ backend/autoupdate/update_mode_darwin.go
@@ -8,7 +8,9 @@ func init() {
 	registerUpdateType("standalone", func() UpdateType {
 		return UpdateType{
 			ArtifactName: "SatisfactoryModManager_darwin_universal.zip",
-			Apply:        apply.MakeDarwinAppApply(),
+			Apply: apply.MakeDarwinAppApply(apply.DarwinApplyConfig{
+				AppName: "SatisfactoryModManager",
+			}),
 		}
 	})
 }
